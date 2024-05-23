fdf9a421fc46a7dc6331cedd4311ce238db1878b
Munmun2712 <141683689+Munmun2712@users.noreply.github.com>
2024-05-23 19:07:41 +0530

    added about.php

---
+++ about.php
@@ -0,0 +1,166 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <title>TechSure Design The Future</title>
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="#">TechSure</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+      
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item active">
+              <a class="nav-link" href="about.php">About</a>
+            </li>
+           
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
+                  aria-haspopup="true" aria-expanded="false">
+                  Topics
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <a class="dropdown-item" href="#">Technology</a>
+                  <a class="dropdown-item" href="#">Web Development</a>
+                  <a class="dropdown-item" href="#">Flutter Development</a>
+                  <a class="dropdown-item" href="#">Figma Development</a>
+                  <div class="dropdown-divider"></div>
+                  <a class="dropdown-item" href="#">Machine Learning</a>
+                  <a class="dropdown-item" href="#">Artificial Intelligence</a>
+                </div>
+              </li>
+            <li class="nav-item">
+                <a class="nav-link" href="contact.php">Contact Us</a>
+              </li>
+            
+          </ul>
+          <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+          </form>
+        </div>
+      </nav>
+    
+    <div class="container my-4">
+        <div class="container marketing">
+
+            <!-- Three columns of text below the carousel -->
+            <div class="row">
+                <div class="col-lg-4">
+                    <img src="https://source.unsplash.com/140x140/?person face educated background"
+                        class="bd-placeholder-img rounded-circle img-fluid" alt="">
+                    <h2>Community</h2>
+                    <p>TechSure connects all the communities for the role of their own improvement.
+                        whether you are a student or a professional, it provides the committment towards 
+                        learning and growing in your field.
+                    </p>
+                    <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
+                </div><!-- /.col-lg-4 -->
+                <div class="col-lg-4">
+                    <img src="image/abc.jpeg" class="bd-placeholder-img rounded-circle"
+                        width="140" height="140" alt="">
+                    <h2>Devices</h2>
+                    <p>Devices are manufactured to provide a idea towards your growing hardware and software field.
+                        Improvement held with your growing skills.
+                    </p>
+                    <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
+                </div><!-- /.col-lg-4 -->
+                <div class="col-lg-4">
+                    <img src="https://source.unsplash.com/140x140/?ceo" class="bd-placeholder-img rounded-circle"
+                        width="140" height="140" alt="">
+                    <h2>Development</h2>
+                    <p>TechSure enhance the material with growing algorithms with unbeleivble devlopment in the tech 
+                        industry with automation and learning.
+                    </p>
+                    <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
+                </div><!-- /.col-lg-4 -->
+            </div><!-- /.row -->
+
+
+            <!-- START THE FEATURETTES -->
+
+            <hr class="featurette-divider  d-flex justify-content-center align-items-center">
+
+            <div class="row featurette d-flex justify-content-center align-items-center">
+                <div class="col-md-7">
+                    <h2 class="featurette-heading">It all started with TechSure's Idea. <span class="text-muted">It’ll blow your
+                            mind.</span></h2>
+                    <p class="lead">To provide a materialistics property like education, feeling free towards the world to present 
+                        yourself as a confident candidate. </p>
+                </div>
+                <div class="col-md-5">
+                    <img src="https://source.unsplash.com/400x400/?Cloud security"
+                        class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500"
+                        height="500" alt="">
+
+                </div>
+            </div>
+
+            <hr class="featurette-divider ">
+
+            <div class="row featurette d-flex justify-content-center align-items-center">
+                <div class="col-md-7 order-md-2">
+                    <h2 class="featurette-heading">Oh yeah, it’s that good. <span class="text-muted">See for
+                            yourself.</span></h2>
+                    <p class="lead">Universities provide education on the behalf of syllabus.we will provide you education on 
+                        behalf of your consideration towards learning , character building , mindset and development.
+                    </p>
+                </div>
+                <div class="col-md-5 order-md-1">
+                    <img src="https://source.unsplash.com/400x400/? AWS development"
+                        class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500"
+                        height="500" alt="">
+
+                </div>
+            </div>
+
+            <hr class="featurette-divider  ">
+
+            <div class="row featurette d-flex justify-content-center align-items-center">
+                <div class="col-md-7">
+                    <h2 class="featurette-heading">We started TechSure in 2024 <span class="text-muted">It's a Checkmate.</span></h2>
+                    <p class="lead">A startup from a classroom of university , a layout and creation of six friends
+                        , motivated enough to build a sur field in the tech world.
+                    </p>
+                </div>
+                <div class="col-md-5">
+                    <img src="https://source.unsplash.com/400x400/?Microsoft "
+                        class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500"
+                        height="500" alt="">
+
+                </div>
+            </div>
+        </div>
+        <footer class="container my-4">
+            <p class="float-right"><a href="#">Back to top</a></p>
+            <p>© 2024-2025 TechSure, Inc. · <a href="#">Privacy</a> · <a href="#">Terms</a></p>
+          </footer>
+        <!-- Optional JavaScript -->
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+            integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+            crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js"
+            integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+            crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js"
+            integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+            crossorigin="anonymous"></script>
+</body>
+
+</html>
