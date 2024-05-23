0934732f358fc77827a38942cb03a2e00b81551c
Munmun2712 <141683689+Munmun2712@users.noreply.github.com>
2024-05-23 19:05:13 +0530

    added

---
+++ index.php
@@ -0,0 +1,198 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css" crossorigin="anonymous">
+    
+    <title>TechSure Design The Future</title>
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="#">TechSure</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+      
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item ">
+              <a class="nav-link" href="about.php">About</a>
+            </li>
+           
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
+                  aria-haspopup="true" aria-expanded="false">
+                  Topics
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <a class="dropdown-item" href="#">Technology</a>
+                  <a class="dropdown-item" href="#">Web Development</a>
+                  <a class="dropdown-item" href="#">Flutter Development</a>
+                  <a class="dropdown-item" href="#">Figma Development</a>
+                  <div class="dropdown-divider"></div>
+                  <a class="dropdown-item" href="#">Machine Learning</a>
+                  <a class="dropdown-item" href="#">Artificial Intelligence</a>
+                </div>
+              </li>
+            <li class="nav-item">
+                <a class="nav-link" href="contact.php">Contact Us</a>
+              </li>
+            
+          </ul>
+          <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+          </form>
+        </div>
+      </nav>
+      <div class="bd-example">
+        <div id="carouselExampleCaptions" class="carousel slide carousel-fade" data-ride="carousel">
+          <ol class="carousel-indicators">
+            <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+            <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+            <li data-target="#carouselExampleCaptions" data-slide-to="3"></li>
+            <li data-target="#carouselExampleCaptions" data-slide-to="4"></li>
+          </ol>
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <img src="https://source.unsplash.com/1400x400/?Technology" class="d-block w-100" alt="...">
+              <div class="carousel-caption d-none d-md-block">
+                <h2>Welcome To TechSure</h2>
+                <p>A well formal trend suiting website</p>
+                <button class="btn btn-danger">Technology</button>
+                <button class="btn btn-success">Web Devlopment</button>
+                <button class="btn btn-primary">Tech Fun</button>
+              </div>
+            </div>
+            <div class="carousel-item">
+              <img src="https://source.unsplash.com/1400x400/?google python,coding" class="d-block w-100" alt="...">
+              <div class="carousel-caption d-none d-md-block">
+                <h2>Coding Fundamentals</h2>
+                <p>Learn , Build , Devlop ,Trending focus on programming .</p>
+                <button class="btn btn-danger">Technology</button>
+                <button class="btn btn-success">Web Devlopment</button>
+                <button class="btn btn-primary">Tech Fun</button>
+              </div>
+            </div>
+            <div class="carousel-item">
+              <img src="https://source.unsplash.com/1400x400/?android devlopment" class="d-block w-100" alt="...">
+              <div class="carousel-caption d-none d-md-block">
+                <h2>Futuristic scope</h2>
+                <p>Clearity of thoughts and vision provides you the stairs of success.</p>
+                <button class="btn btn-danger">Technology</button>
+                <button class="btn btn-success">Web Devlopment</button>
+                <button class="btn btn-primary">Tech Fun</button>
+              </div>
+            </div>
+            <div class="carousel-item">
+                <img src="https://source.unsplash.com/1400x400/?mac developer" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                  <h2>Join the Apple </h2>
+                  <p>Design your personality and improve like a IOS device.</p>
+                  <button class="btn btn-danger">Technology</button>
+                <button class="btn btn-success">Web Devlopment</button>
+                <button class="btn btn-primary">Tech Fun</button>
+                </div>
+              </div>
+              <div class="carousel-item">
+                <img src="https://source.unsplash.com/1400x400/?Artificial Intelligence" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                  <h2>Artificial Intelligence</h2>
+                  <p>Choose a career to surprise yourself with the human creation and being a god for them.</p>
+                  <button class="btn btn-danger">Technology</button>
+                <button class="btn btn-success">Web Devlopment</button>
+                <button class="btn btn-primary">Tech Fun</button>
+                </div>
+              </div>
+          </div>
+          <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+          </a>
+        </div>
+      </div>
+     <div class="container my-4">
+      <div class="row mb-2">
+        <div class="col-md-6">
+          <div class="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+            <div class="col p-4 d-flex flex-column position-static">
+              <strong class="d-inline-block mb-2 text-primary">Blogs</strong>
+              <h3 class="mb-0">WorkSpace Meetings</h3>
+              <div class="mb-1 text-muted">Nov 12</div>
+              <p class="card-text mb-auto">The following work culture in offices before the covid19 and after 2020 it was dependent on web cameras.</p>
+              <a href="#" class="stretched-link">Continue reading</a>
+            </div>
+            <div class="col-auto d-none d-lg-block">
+              <img src="https://source.unsplash.com/400x400/?Microsoft azure development" class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" alt="">
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6">
+          <div class="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+            <div class="col p-4 d-flex flex-column position-static">
+              <strong class="d-inline-block mb-2 text-success">Design</strong>
+              <h3 class="mb-0">Visual Effects</h3>
+              <div class="mb-1 text-muted">Nov 11</div>
+              <p class="mb-auto">Depending on UI/UX Design a balance between visual effects and structural layouts .</p>
+              <a href="#" class="stretched-link">Continue reading</a>
+            </div>
+            <div class="col-auto d-none d-lg-block">
+              <img class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" src="https://source.unsplash.com/400x400/?visualization" alt="">
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-6">
+          <div class="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+            <div class="col p-4 d-flex flex-column position-static">
+              <strong class="d-inline-block mb-2 text-secondary">Machines</strong>
+              <h3 class="mb-0">Effective Requirments</h3>
+              <div class="mb-1 text-muted">Nov 10</div>
+              <p class="mb-auto">Alatest updation of changing the requirements of a employee and programmer .</p>
+              <a href="#" class="stretched-link">Continue reading</a>
+            </div>
+            <div class="col-auto d-none d-lg-block">
+              <img class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" src="https://source.unsplash.com/400x400/?computers with compilers" alt="">
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-6">
+          <div class="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+            <div class="col p-4 d-flex flex-column position-static">
+              <strong class="d-inline-block mb-2 text-warning">Project Analysis</strong>
+              <h3 class="mb-0">Project Management</h3>
+              <div class="mb-1 text-muted">Nov 9</div>
+              <p class="mb-auto">Leading a team for the revenue and satisfaction of clients for more growing results .</p>
+              <a href="#" class="stretched-link">Continue reading</a>
+            </div>
+            <div class="col-auto d-none d-lg-block">
+              <img class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" src="https://source.unsplash.com/400x400/?software projects" alt="">
+            </div>
+          </div>
+        </div>
+      </div>
+     </div>
+     <footer class="container my-4">
+      <p class="float-right"><a href="#">Back to top</a></p>
+      <p>© 2024-2025 TechSure, Inc. · <a href="#">Privacy</a> · <a href="#">Terms</a></p>
+    </footer>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
